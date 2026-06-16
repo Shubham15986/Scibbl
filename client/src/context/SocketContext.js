3ae205fb1c8926080935f4661da691e8223a8749
Shubham15986 <shubham15986@gmail.com>
2026-06-16 20:43:06 +0530

    Fix unused variable to pass Vercel build

--- client/src/context/SocketContext.js
+++ client/src/context/SocketContext.js
@@ -20,7 +20,7 @@ export const useSocket = () => {
 
 
 export const SocketProvider = ({ children }) => {
-  const [isConnected, setIsConnected] = useState(socket.connected);
+  const [, setIsConnected] = useState(socket.connected);
 
   useEffect(() => {
     socket.on('connect', () => {
